ca7b7dfbb03036242afebf9a7cfeeff732cda85c
chercheri <mohamed.chercheri@yahoo.com>
2019-09-02 02:30:45 +0100

    add/edit/delete Produit done + resolver

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router'
 
 import { ProduitService } from "./produit.service";
 import { Produit } from "../shared/produit";
@@ -14,18 +15,26 @@ export class ProduitComponent implements OnInit{
 
   produits : Produit[];
   produitForm : FormGroup;
+  operation : string = "add";
+  selectedProduit : Produit ;
 
-  constructor(private produitService : ProduitService, private fb : FormBuilder){
-    this.produitForm = fb.group({
-      reference: ['',Validators.required],
-      quantite: '',
-      prixUnitaire:''
-    });
+  constructor(private produitService : ProduitService, private fb : FormBuilder, private route : ActivatedRoute){
+    this.createForm()
 
   }
 
   ngOnInit(){
-    this.loadProduits();
+    this.initProduit();
+    //this.loadProduits();
+    this.produits = this.route.snapshot.data.produits
+  }
+
+  createForm(){
+    this.produitForm = this.fb.group({
+      reference: ['',Validators.required],
+      quantite: '',
+      prixUnitaire:''
+    });
   }
 
   controlerReference():boolean{
@@ -39,5 +48,40 @@ export class ProduitComponent implements OnInit{
       () => {console.log('chargement des produits terminé.')}
     );
   }
+
+  addProduit(){
+    const p = this.produitForm.value;
+    this.produitService.addProduit(p).subscribe(
+      res => {
+        this.initProduit();
+        this.loadProduits();
+      }
+    );
+  }
+
+  updateProduit(){
+    this.produitService.updateProduit(this.selectedProduit).subscribe(
+      res => {
+        this.initProduit();
+        this.loadProduits();
+      }
+    );
+  }
+
+  deleteProduit(){
+    this.produitService.deleteProduit(this.selectedProduit.reference).subscribe(
+      res => {
+        this.initProduit();
+        this.loadProduits();
+      },
+
+    );
+  }
+
+  initProduit(){
+    this.selectedProduit = new Produit();
+    this.createForm();
+  }
+
 
 }
--- src/app/produit/produit.service.ts
+++ src/app/produit/produit.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { API_URLS } from '../config/api.url.config';
+import { Produit } from '../shared/produit';
 
 @Injectable()
 export class ProduitService{
@@ -11,5 +12,14 @@ export class ProduitService{
   }
   getProduits(): Observable<any> {
     return this.http.get(API_URLS.PRODUIT_URL);
+  }
+  addProduit(p : Produit): Observable<any> {
+    return this.http.post(API_URLS.PRODUIT_URL, p);
+  }
+  updateProduit(p : Produit): Observable<any> {
+    return this.http.put(API_URLS.PRODUIT_URL, p);
+  }
+  deleteProduit(reference : string): Observable<any> {
+    return this.http.delete(API_URLS.PRODUIT_URL+ `/${reference}`);
   }
 }
